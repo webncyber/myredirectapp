9a8922af2752c9a3f9a26772e881bfd7f7d7709d
nazo <webncyber@yahoo.com>
2024-06-17 08:28:19 -0400

    setup welcome page

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,78 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
+const nextConfig = {
+    async redirects() {
+      return [
+        {
+          source: '/flyers/dr-strings-nickel-lo-rider',
+          destination: '/',
+          permanent: true,
+        },
+        {
+          source: '/blogs/baby-must-haves/BabyBjorn-small-baby-bib',
+          destination: '/',
+          permanent: true,
+        },
+        {
+          source: '/blogs/back-to-school/large-college-backpack',
+          destination: '/',
+          permanent: true,
+        },
+        {
+          source: '/blogs/baby-must-haves/retractable-baby-gate',
+          destination: '/',
+          permanent: true,
+        },
+        {
+          source: '/blogs/back-to-school/shower-caddy-tote-bag',
+          destination: '/',
+          permanent: true,
+        },
+        {
+          source: '/blogs/home-decor/facts-on-having-a-cozy-home',
+          destination: '/',
+          permanent: true,
+        },
+        {
+          source: '/blogs/healthy-recipes/top-5-brunch-ideas',
+          destination: '/',
+          permanent: true,
+        },
+        {
+          source: '/blogs/baby-must-haves/baby-electric-nail-trimmer',
+          destination: '/',
+          permanent: true,
+        },
+        {
+          source: '/blogs/gardening/leaf-sweepers',
+          destination: '/',
+          permanent: true,
+        },
+        {
+          source: '/blogs/back-to-school',
+          destination: '/',
+          permanent: true,
+        },
+        {
+          source: '/blogs/healthy-recipes',
+          destination: '/',
+          permanent: true,
+        },
+        {
+          source: '/blogs/home-decor',
+          destination: '/',
+          permanent: true,
+        },
+        {
+          source: '/blogs/baby-must-haves',
+          destination: '/',
+          permanent: true,
+        },
+        {
+          source: '/blogs/gardening',
+          destination: '/',
+          permanent: true,
+        },
+      ]
+    }
+  }
 export default nextConfig;
