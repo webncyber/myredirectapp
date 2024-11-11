9581c60f31c7f480e504c1f874e816ed9a0b0872
nazo <webncyber@yahoo.com>
2024-11-10 22:46:25 -0500

    update brower title

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,8 +4,8 @@ import GoogleAnalytics from "./components/googleAnalytics/ga";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "My Research Center",
-  description: "MyResearchCenter.com Domain for sale",
+  title: process.env.NEXT_PUBLIC_SITE_DOMAIN,
+  description: "Domain for sale",
   keywords: "buy domain, domain for sale, premium domain, domain marketplace, brandable domain, domain auction, domain investment, domain name, digital assets, online business, research center, research resources, academic research, scientific studies, research tools"
 };
 
